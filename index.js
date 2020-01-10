208d1923327da258c18a0b3dc5f11c5f3636d7e2
stelthdroid8 <brandon.kennedy1994@gmail.com>
2020-01-10 15:18:52 -0500

    add inital CLI setup via caporal

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 #!/usr/bin/env node
+const debounce = require('lodash.debounce');
+const chokidar = require('chokidar');
+const prog = require('caporal');
+
+prog
+  .version('0.0.1')
+  .argument('[filename]', 'Name of a file to execute')
+  .action(({ filename }) => {
+    const name = filename || 'index.js';
+    const start = debounce(() => {
+      console.log('starting');
+    }, 100);
+    chokidar
+      .watch('.')
+      .on('add', start)
+      .on('change', start)
+      .on('unlink', start);
+  });
+
+prog.parse(process.argv);
